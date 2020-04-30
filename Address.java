2d16f6c7060be3c3cdfb3ff0ae5376ecef56da04
ethansky <fragglekids@gmail.com>
2020-04-29 23:38:53 -0500

    reorganize methods

--- Address.java
+++ Address.java
@@ -20,44 +20,44 @@ public class Address {
         return homeNumber;
     }
 
-    public void setHomeNumber(int homeNumber) {
-        this.homeNumber = homeNumber;
-    }
-
     public String getStreet() {
         return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getState() {
         return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     public int getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
-
     public String getCountry() {
         return country;
+    }
+
+    public void setHomeNumber(int homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
     }
 
     public void setCountry(String country) {
