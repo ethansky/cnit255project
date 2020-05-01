1c0de9e7107ca645861d7418c687fc1867d65a7a
ethansky <fragglekids@gmail.com>
2020-05-01 16:32:11 -0500

    fixed Salaried explanation

--- Salaried.java
+++ Salaried.java
@@ -1,5 +1,5 @@
 public interface Salaried {
-    // Employees aren't always salaried/paid member. They could be volunteers.
+    // Employees aren't always salaried. They could be volunteers or part time.
     void setSalary(double salary);
     double getSalary();
 }
